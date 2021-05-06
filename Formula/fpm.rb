adf94b52099993b9ff3e3d57b045bfb9a76ebffa
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2021-05-06 11:55:24 +0000

    fpm: add 0.2.0 bottle.

--- Formula/fpm.rb
+++ Formula/fpm.rb
@@ -5,6 +5,12 @@ class Fpm < Formula
   sha256 "79d5041f5cebd1adff999017b3b5f88d8814267dbd0faaa0f7c720411ede463e"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/awvwgk/homebrew-fpm/releases/download/fpm-0.2.0"
+    rebuild 1
+    sha256 cellar: :any, catalina: "e3801e6c864bd26c98c060d7be2eaa4c885211f6549fd7c27948304080894f89"
+  end
+
   depends_on "gcc" # for gfortran
   fails_with gcc: "4"
   fails_with gcc: "5"
