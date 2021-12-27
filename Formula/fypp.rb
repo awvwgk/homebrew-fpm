3865e149d36637be1fda6d572f6e36b8d475f5cd
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2021-12-27 23:04:32 +0000

    fypp: add 3.1 bottle.

--- Formula/fypp.rb
+++ Formula/fypp.rb
@@ -8,6 +8,12 @@ class Fypp < Formula
   license "BSD-2-Clause"
   head "https://github.com/aradi/fypp.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/awvwgk/homebrew-fpm/releases/download/fypp-3.1"
+    sha256 cellar: :any_skip_relocation, big_sur:  "4df9140ca392b4e246281ef980914d8ba97a5cceb521146de79e6771c35514ba"
+    sha256 cellar: :any_skip_relocation, catalina: "0e130c3e46041ef6efe8cbbbbf21b1cd80f21aac2f4901489702e2fd1ea32719"
+  end
+
   depends_on "python"
 
   def install
