65131e24528969eb3eafe9691a87843ee5fd2a1c
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2021-12-28 11:01:16 +0100

    Update formula for LFortran
    Closes #10.
    
    Signed-off-by: Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>

--- Formula/lfortran.rb
+++ Formula/lfortran.rb
@@ -4,6 +4,7 @@ class Lfortran < Formula
   url "https://lfortran.github.io/tarballs/release/lfortran-0.14.0.tar.gz"
   sha256 "fc3c1d592c56ae2636065ec0228db747f154f65a0867f6311bc8091efd5c13a7"
   license "BSD-3-Clause"
+  revision 1
 
   bottle do
     root_url "https://github.com/awvwgk/homebrew-fpm/releases/download/lfortran-0.14.0"
@@ -17,7 +18,10 @@ class Lfortran < Formula
   depends_on "zlib"
 
   def install
-    system "cmake", *std_cmake_args, "-G", "Ninja", "-B", "build"
+    cmake_args = std_cmake_args
+    cmake_args << "-DCMAKE_CXX_FLAGS_RELEASE=-O3 -funroll-loops -DNDEBUG"
+    cmake_args << "-DWITH_LLVM=ON"
+    system "cmake", *cmake_args, "-G", "Ninja", "-B", "build"
     system "cmake", "--build", "build"
     system "ctest", "--test-dir", "build", "--output-on-failure"
     system "cmake", "--install", "build"
@@ -25,5 +29,13 @@ class Lfortran < Formula
 
   test do
     system "#{bin}/lfortran", "--version"
+    (testpath/"hello.f90").write <<~EOS
+      program hello
+        print *, "Hello, World!"
+      end
+    EOS
+    system "#{bin}/lfortran", testpath/"hello.f90", "-o", testpath/"hello"
+    assert_predicate testpath/"hello", :exist?
+    system testpath/"hello"
   end
 end
